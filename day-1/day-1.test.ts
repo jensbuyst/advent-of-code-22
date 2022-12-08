c142466393d9a14fc2ad48bbbd325fd01e7cb615
Jens Buyst <jensbuyst@gmail.com>
2022-12-08 19:27:31 +0100

    read input file in test

--- day-1/day-1.test.ts
+++ day-1/day-1.test.ts
@@ -1,7 +1,12 @@
+import {readFileSync} from "fs";
 import day1 from "./day-1";
 
+let input: string[] = readFileSync('./day-1-input.txt', 'utf-8').split(/\r?\n/);
+
 describe('Counting calories', function () {
+
     it('should blabla', function () {
         expect(day1().countingCalories).toEqual(0)
+        console.log(input);
     });
 });
