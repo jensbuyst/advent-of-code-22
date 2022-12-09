dbef659d2d27be21ed55a5e111dbb0316df54aa8
Jens Buyst <jensbuyst@gmail.com>
2022-12-09 16:27:47 +0100

    add read file test + split input in different elves

--- day-1/day-1.test.ts
+++ day-1/day-1.test.ts
@@ -1,30 +1,16 @@
-import {countCalories} from "./day-1";
+import {readCaloriesFromFile, splitInElves} from "./day-1";
 
-const GIVEN_EXAMPLE = [
-    "1000",
-    "2000",
-    "3000",
-    "",
-    "4000",
-    "",
-    "5000",
-    "6000",
-    "",
-    "7000",
-    "8000",
-    "9000",
-    "",
-    "10000"
-]
-describe('Counting calories', function () {
-    describe('of an elf', function () {
-        it('with 1 piece of food', function () {
-            expect(countCalories(["1000"])).toEqual(1000)
-        });
-        it('with 2 pieces of food', function () {
-            expect(countCalories(["1000", "2000"])).toEqual(3000)
-        });
-    });
+describe("Read file", () => {
+    it("and parse strings to numbers", () => {
+        const file = readCaloriesFromFile();
+        expect(file).toEqual([1000, 1000, NaN, 3000])
 
-});
+    })
+})
+
+describe("Elf creator", () => {
+    it("creates Elves from calorie list", () => {
+        expect(splitInElves( [1000, 1000, NaN, 2000])).toEqual([[1000, 1000], [2000]])
+    })
+})
 
--- day-1/day-1.ts
+++ day-1/day-1.ts
@@ -1,8 +1,23 @@
+import {readFileSync} from "fs";
 
-export const countCalories = (elf: string[]): number => {
-    const numberValuesOfCalories = elf.map(food => parseInt(food))
+export const readCaloriesFromFile = () => {
+    const file = readFileSync('./testFile.txt', 'utf-8').split(/\r?\n/);
+    return file.map(calorie => parseInt(calorie));
+};
 
-    return numberValuesOfCalories.reduce((prev, current) => prev + current)
+export function splitInElves(input: number[]) {
+    return input.reduce((acc, curr, index) => {
+        if(acc.length === 0) {
+            acc.push([])
+        }
+
+        if(Number.isNaN(curr)) {
+            acc.push([])
+            return acc;
+        }
+
+        acc[acc.length - 1].push(curr)
+
+        return acc;
+    }, [] as number[][])
 }
-
-
